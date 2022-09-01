60160434489118f1257fcd83ab8c33ce1562d6ba
Alexander Kraev <kraev_as@utkonos.ru>
2022-09-01 17:06:15 +0300

    add swiftpm support

--- Package.swift
+++ Package.swift
@@ -15,5 +15,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "HexColors"
+    name: "HexColors",
+     platforms: [
+         .iOS(.v11),
+         .macOS(.v10_15),
+         .tvOS(.v13)
+     ],
+     products: [
+         .library(
+             name: "HexColors",
+             targets: ["HexColors"]),
+         .library(
+             name: "HexColors-Static",
+             type: .static,
+             targets: ["HexColors"]),
+         .library(
+             name: "HexColors-Dynamic",
+             type: .dynamic,
+             targets: ["HexColors"]),
+     ],
+     dependencies: [],
+     targets: [
+        .target(
+            name: "HexColors",
+            dependencies: [],
+            path: "Sources"
+        )
+     ]
 )
